f4d8afe12f6f7038a79179bea7c176cd94cb71cd
Hector <hr9457@gmail.com>
2025-11-25 12:36:15 -0600

    first commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  @MinLength(1)
+  name: string;
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+}
---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,14 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument } from 'mongoose';
+
+export type PokemonDocument = HydratedDocument<Pokemon>;
+
+@Schema()
+export class Pokemon {
+  @Prop({ unique: true, index: true })
+  name: string;
+  @Prop({ unique: true, index: true })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
